e0ce6ded5288e44c1c8dc7b54f8087fac33ab275
elijah-gronlund <elijah.gronlund@mymail.champlain.edu>
2026-02-19 10:19:31 -0500

    added ssecure-ssh.sh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+# seccure-ssh.sh
+# Author elijah-gronlund
+# Creates a new ssh user using $1 parameter 
+# Adds a public key from the local repo or curled from the remote repo 
+# Removes root abiility to log in with ssh
+echo "ALL CODE GOES HERE"
