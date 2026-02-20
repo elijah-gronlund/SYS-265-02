23596bfa750daafead641a3486c79f191860933d
elijah-gronlund <elijah.gronlund@mymail.champlain.edu>
2026-02-20 02:06:57 -0500

    Revise secure-ssh.sh for user creation and key setup
    Updated script to correct author name and comments, and added functionality for user creation and SSH key setup.

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,6 +1,25 @@
-# seccure-ssh.sh
-# Author elijah-gronlund
-# Creates a new ssh user using $1 parameter 
-# Adds a public key from the local repo or curled from the remote repo 
-# Removes root abiility to log in with ssh
-echo "ALL CODE GOES HERE"
+#!/bin/bash
+
+# secure-ssh.sh
+# author Elijah
+# adds a public key from the local repo or curled from the remote repo 
+# removes roots ability to ssh in
+
+# Creates a user and adds the public key
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/elijah/SYS-265-02
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+# Blocking root ssh login
+if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+   sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+else
+   echo "PermitRootLogin not found in /etc/ssh/sshd_config"
+fi
+
+# Restart SSH
+sudo systemctl restart sshd.service
